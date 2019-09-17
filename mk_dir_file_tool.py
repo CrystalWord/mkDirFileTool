39ccbf40c4de2db80b5412c30c038be8ca62e7cf
zhangjing <1134794665.com>
2019-09-17 10:56:34 +0800

    自动创建常用文件（一些重复性内容），并增加了手动输入创建文件功能

--- mk_dir_file_tool.py
+++ mk_dir_file_tool.py
@@ -11,11 +11,42 @@ try:
 except:
     pass
 
-l = ["笔记.md", "创建py文件.py", "计算代码量.py", "test.py"]
+
+def mk_md_file():
+    with open('./%s/%s.md' % (t_now, file_name), 'wb') as f:
+        f.write("# Web应用开发技术\n".encode())
+        f.close
+
+
+def mk_py_file():
+    with open('./%s/%s.py' % (t_now, file_name), 'wb') as f:
+        f.write("#!/usr/bin/python3\n\
+# -*- coding: utf-8 -*-\n\n\
+".encode())
+        f.close()   
+
+def mk_html_file():
+    with open('./%s/%s.html' % (t_now, file_name), 'wb') as f:
+        f.write('<!DOCTYPE html>\n\
+<html>\n\
+    <head>\n\
+        <meta charset="utf-8">\n\
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">\n\
+        <title></title>\n\
+    </head>\n\
+    <body>\n\
+        <div>\n\n\
+        </div>\n\
+    </body>\n\
+</html>\
+'.encode())
+        f.close()   
+
+l = ["笔记.md", "创建py文件.py", "计算代码量.py", "test.py", "test.html"]
 for i in l:
     with open('./%s/%s' % (t_now, i), 'ab') as f:
         if i == "笔记.md":
-            f.write("# Python高级\n".encode())
+            f.write("# Web应用开发技术\n".encode())
             f.close
         elif i == "创建py文件.py":
             f.write("#!/usr/bin/python3\n\
@@ -51,10 +82,36 @@ while True:\n\
         s -= (24 + 10)\n\n\
     print('今日代码量：%s行' % s)".encode())
             f.close()
-        elif i == "test.py":
+        elif i == "test.py": 
             f.write("#!/usr/bin/python3\n\
 # -*- coding: utf-8 -*-\n\n\
 ".encode())
-            f.close()  
-    print(i + "创建成功！")    
+            f.close() 
+        elif i == "test.html":
+            f.write('<!DOCTYPE html>\n\
+<html>\n\
+    <head>\n\
+        <meta charset="utf-8">\n\
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">\n\
+        <title></title>\n\
+    </head>\n\
+    <body>\n\
+        <div>\n\n\
+        </div>\n\
+    </body>\n\
+</html>\
+'.encode())
+            f.close()   
+
+    print("文件%s创建成功!" % i)  
     time.sleep(1)
+
+while True:
+    file_name = input("请输入文件名：")
+    file_type = input("请输入文件类型：")
+    if file_type == 'md':
+        mk_md_file()
+    elif file_type == 'py':
+        mk_py_file()
+    elif file_type == 'html':
+        mk_html_file()
